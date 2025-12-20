87540f74714e768403d3bad64face30fd51c566a
deya <deyahato19@gmail.com>
2025-12-20 16:18:18 +0000

    add script

---
+++ script.sh
@@ -0,0 +1,4 @@
+git add . && git commit -m "Token Comment" && git push origin main
+
+curl -u "deyahh:11a7412ff80ae08f67fa179aafdc8ad352" \
+     "http://192.168.1.104:8080/job/job1/build?token=11a7412ff80ae08f67fa179aafdc8ad352"
