61fdcbcc3374f6b8cdc3ce1cf695c5741c9b7204
deya <deyahato19@gmail.com>
2025-12-25 23:21:23 +0200

    Delete

--- script.sh
+++
@@ -1,4 +0,0 @@
-git add . && git commit -m "Token Comment" && git push origin main
-
-curl -u "deyahh:11a7412ff80ae08f67fa179aafdc8ad352" \
-"http://192.168.1.104:8080/job/restaurant-project/build?token=11a7412ff80ae08f67fa179aafdc8ad352"
