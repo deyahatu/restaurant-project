6256a52afc332fd0b8fbc887d95566a20118d284
deyahatu <deyahato19@gmail.com>
2025-12-29 10:11:12 +0000

    t

--- site/index.php
+++ site/index.php
@@ -60,7 +60,7 @@ $conn->close();
         }
 
         .card {
-            background: white;
+            background: red;
             width: 420px;
             padding: 25px;
             border-radius: 15px;
